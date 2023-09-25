f2d23a65df2bfaadcf6532d9ce6701ed272a6628
Blye <adam.smielewski@gmail.com>
2023-09-25 14:58:41 +0200

    Renamed project and app

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PmsApiConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pms_api'
+    name = 'api'
