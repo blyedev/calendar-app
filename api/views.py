09271ab0a0dc3f54ba81117651d74620f5f832ae
Blye <adam.smielewski@gmail.com>
2023-09-25 18:31:14 +0200

    Made a skeleton rest api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import TreeNode, Event, Task, Project, Group
+from .serializers import TreeNodeSerializer, EventSerializer, TaskSerializer, ProjectSerializer, GroupSerializer
 
 # Create your views here.
+
+class TreeNodeListView(generics.ListAPIView):
+    queryset = TreeNode.objects.all()
+    serializer_class = TreeNodeSerializer
+
+class EventListView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class ProjectListView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class GroupListView(generics.ListAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    
