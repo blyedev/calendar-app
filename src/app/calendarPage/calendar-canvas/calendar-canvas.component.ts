5c989bf0208fa518cfd7f60e6fb97a9e02fdbbde
Blye <adam.smielewski@gmail.com>
2023-09-28 02:37:41 +0200

    Minor code refactor

--- src/app/calendarPage/calendar-canvas/calendar-canvas.component.ts
+++ src/app/calendarPage/calendar-canvas/calendar-canvas.component.ts
@@ -34,7 +34,7 @@ export class CalendarCanvasComponent implements OnInit {
 
   getShortEventsByDay(events: CalendarEvent[], dayOfWeekIndex: number): CalendarEvent[] {
     // Creates the date object for the start of week
-    const { dayStart, dayEnd } = this.getDayBoundries(dayOfWeekIndex);
+    const { dayStart, dayEnd } = this.getDayBounds(dayOfWeekIndex);
 
     const filteredEvents = events.filter(event => {
       // filters out events longer than a day
@@ -49,15 +49,27 @@ export class CalendarCanvasComponent implements OnInit {
     return filteredEvents
   }
 
-  getDayBoundries(dayOfWeekIndex: number): { dayStart: Date, dayEnd: Date } {
-    const currentWeekStart = new Date();
+  getWeekBounds(): { weekStart: Date, weekEnd: Date } {
+    const currentDate = new Date();
+
+    const currentWeekStart = new Date(currentDate);
     const mondayOffset = currentWeekStart.getDay() > 0 ? currentWeekStart.getDay() - 1 : 6; // 0 (Sunday) to 6 (Saturday)
     currentWeekStart.setDate(currentWeekStart.getDate() - mondayOffset);
     currentWeekStart.setHours(0, 0, 0, 0); // Midnight of Monday
 
+    const currentWeekEnd = new Date(currentWeekStart);
+    currentWeekEnd.setDate(currentWeekEnd.getDate() + 7);
+    currentWeekEnd.setHours(0, 0, 0, 0); // Midnight after Sunday
+
+    return { weekStart: currentWeekStart, weekEnd: currentWeekEnd };
+  }
+
+  getDayBounds(dayOfWeekIndex: number): { dayStart: Date, dayEnd: Date } {
+    const { weekStart } = this.getWeekBounds();
+
 
     // Creates the start and end date objects from the weekStart and the dayindex parameter
-    const dayStart = new Date(currentWeekStart);
+    const dayStart = new Date(weekStart);
     dayStart.setDate(dayStart.getDate() + dayOfWeekIndex);
     const dayEnd = new Date(dayStart);
     dayEnd.setDate(dayEnd.getDate() + 1);
