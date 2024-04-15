856ad2035cc5a93e688be4c6899a155008923236
Blyedev <adam.smielewski@gmail.com>
2024-04-15 23:40:45 +0200

    Fixed delete functionality

--- src/app/modules/calendar/week-container/event/event.component.ts
+++ src/app/modules/calendar/week-container/event/event.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { CalendarEvent } from 'src/app/core/models/calendar-event';
+import { CalendarEventService } from 'src/app/core/services/calendar-event-service/calendar-event.service';
 
 @Component({
   selector: 'app-event',
@@ -8,4 +9,28 @@ import { CalendarEvent } from 'src/app/core/models/calendar-event';
 })
 export class EventComponent {
   @Input({ required: true }) event!: CalendarEvent;
+
+  constructor(
+    private calendarEventService: CalendarEventService) { }
+
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Delete') {
+      // Handle the delete key press
+      console.log('Delete key pressed inside the component.');
+
+      // Call the deleteEvent method from your service
+      this.calendarEventService.deleteEvent(this.event.id).subscribe({
+        next: () => {
+          console.log('Event deleted successfully');
+          this.calendarEventService.refreshEvents();
+        },
+        error: (error) => console.error('Error deleting event:', error),
+        complete: () => {
+          // Additional logic if needed
+        }
+      });
+    }
+  }
 }
