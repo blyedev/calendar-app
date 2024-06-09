9453392f39c031cba089dc09523cfcecac52d2b8
blyedev <adam.smielewski@gmail.com>
2024-06-09 17:42:03 +0200

    Reworking github actions along with necessary adjustments to source codes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,38 +1,51 @@
 from rest_framework import generics
 from .models import TreeNode, Event, Task, Project, Group
-from .serializers import TreeNodeSerializer, EventSerializer, TaskSerializer, ProjectSerializer, GroupSerializer
+from .serializers import (
+    TreeNodeSerializer,
+    EventSerializer,
+    TaskSerializer,
+    ProjectSerializer,
+    GroupSerializer,
+)
 
 # Create your views here.
+
 
 class TreeNodeListView(generics.ListAPIView):
     queryset = TreeNode.objects.all()
     serializer_class = TreeNodeSerializer
 
+
 class EventListView(generics.ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
 
 class EventCreateView(generics.CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+
 class EventUpdateView(generics.UpdateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
 
 class EventDeleteView(generics.DestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+
 class TaskListView(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class ProjectListView(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+
 class GroupListView(generics.ListAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    
