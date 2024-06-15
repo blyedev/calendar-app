46950a27d23ca57be5075fb7ee55290fed393b20
blyedev <adam.smielewski@gmail.com>
2024-06-15 22:02:15 +0200

    Added healthchecks

--- backend/pms_api/urls.py
+++ backend/pms_api/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("ht/", include("health_check.urls")),
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="api/", permanent=False)),
     path("api/", include("api.urls")),
