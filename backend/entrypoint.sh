2929f3a5affb598c6fd37efd403d991489b67937
blyedev <adam.smielewski@gmail.com>
2024-06-28 17:57:49 +0200

    Removing printf debugging

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Run migrations
 python manage.py migrate
-
-# Attempt to create the superuser
-echo "Creating superuser 'root'..."
-python manage.py createsuperuser --noinput --username root --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser 'root' already exists."
-
-# Check if the superuser exists
-SUPERUSER_EXISTS=$(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='root').exists())")
-
-echo "Superuser credentials:"
-echo "Username: root"
-echo "Email: $DJANGO_SUPERUSER_EMAIL"
-echo "Password: $DJANGO_SUPERUSER_PASSWORD"
-
-if [ "$SUPERUSER_EXISTS" == "True" ]; then
-  echo "Superuser 'root' exists."
-else
-  echo "Failed to create superuser 'root'."
-fi
-
-# Start the application
+python manage.py createsuperuser --noinput --username root
 gunicorn calendar_project.wsgi:application --bind 0.0.0.0:8000
