8ea1af36c1985c0f12f4e1e719057855a7874612
blyedev <adam.smielewski@gmail.com>
2024-06-28 17:25:06 +0200

    Debugging superuser creation 2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,12 +10,13 @@ python manage.py createsuperuser --noinput --username root --email $DJANGO_SUPER
 # Check if the superuser exists
 SUPERUSER_EXISTS=$(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='root').exists())")
 
+echo "Superuser credentials:"
+echo "Username: root"
+echo "Email: $DJANGO_SUPERUSER_EMAIL"
+echo "Password: $DJANGO_SUPERUSER_PASSWORD"
+
 if [ "$SUPERUSER_EXISTS" == "True" ]; then
   echo "Superuser 'root' exists."
-  echo "Superuser credentials:"
-  echo "Username: root"
-  echo "Email: $DJANGO_SUPERUSER_EMAIL"
-  echo "Password: $DJANGO_SUPERUSER_PASSWORD"
 else
   echo "Failed to create superuser 'root'."
 fi
