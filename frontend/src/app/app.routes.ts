f6a782623e67f43b7cc2af90c65bce6a3b2d8c6f
blyedev <adam.smielewski@gmail.com>
2024-07-02 12:39:44 +0200

    Restructured sample nav to fit best practices

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { authGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import("./home-page/home-page.component").then(m => m.HomePageComponent),
+    loadComponent: () => import("./modules/public-pages/home-page/home-page.component").then(m => m.HomePageComponent),
   },
   {
     path: 'login',
-    loadComponent: () => import("./login-page/login-page.component").then(m => m.LoginPageComponent),
+    loadComponent: () => import("./modules/auth/login-page/login-page.component").then(m => m.LoginPageComponent),
   },
   {
     path: 'calendar',
-    loadComponent: () => import("./calendar-page/calendar-page.component").then(m => m.CalendarPageComponent),
+    loadComponent: () => import("./modules/calendar/calendar-page/calendar-page.component").then(m => m.CalendarPageComponent),
     canActivate: [authGuard]
   },
 ];
