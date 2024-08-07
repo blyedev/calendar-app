ed6c07115bcf99d203287cad4d59a7365be7b696
blyedev <adam.smielewski@gmail.com>
2024-08-07 17:55:36 +0200

    Hotfix faulty migration regenerated, created a more accurate testing env

--- backend/calendar_project/settings/dev.py
+++ backend/calendar_project/settings/dev.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 SECRET_KEY = "django-insecure-&wtt80y-1q89ox=t&u^#s!t3s@%sc(i8uwin77h-5sra5$+y6y"
@@ -8,7 +9,11 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "your_db_name"),
+        "USER": os.environ.get("POSTGRES_USER", "your_db_user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "your_db_password"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
