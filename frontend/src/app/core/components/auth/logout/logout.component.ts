fd4863920c53b902813e850f1c6ca8d4230734ef
blyedev <adam.smielewski@blyedev.com>
2024-12-28 17:00:04 +0100

    feat(auth): create a simple logout page

--- frontend/src/app/core/components/auth/logout/logout.component.ts
+++ frontend/src/app/core/components/auth/logout/logout.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { scan, Subscription, takeWhile, timer } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-logout',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './logout.component.html',
   styleUrl: './logout.component.css',
 })
-export class LogoutComponent implements OnDestroy {
-  subscription: Subscription;
+export class LogoutComponent implements OnInit, OnDestroy {
+  private logoutSubscription: Subscription | undefined;
+
+  countdown$ = timer(0, 1000).pipe(
+    scan((acc) => --acc, 10),
+    takeWhile((x) => x >= 0),
+  );
+  private countdownSubscription: Subscription | undefined;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-  ) {
-    this.subscription = this.authService.deleteSession().subscribe({
+  ) {}
+
+  ngOnInit(): void {
+    this.logoutSubscription = this.authService.deleteSession().subscribe({
       next: (value) => {
         if (value.status === 401) {
-          this.router.navigate(['auth', 'login']);
+          this.countdownSubscription = this.countdown$.subscribe({
+            complete: () => {
+              this.router.navigate(['']);
+            },
+          });
         }
       },
     });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.logoutSubscription?.unsubscribe();
+    this.countdownSubscription?.unsubscribe();
   }
 }
